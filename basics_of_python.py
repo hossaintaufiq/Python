edfab1d7313ccd55e2e38e95fcc13eea38d7d7a3
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2024-12-10 12:04:52 +0600

    python basic

--- basics_of_python.py
+++ basics_of_python.py
@@ -1,1 +1,25 @@
-print("Hello world ")
+# pyton full course from the start 
+
+
+fistName = "Tony "
+lastName= "Stack"
+age=51 ; 
+comment= "Tony is a genius"
+
+
+print(fistName, lastName,age, comment)
+#print("Hello world ")
+
+
+# input variable 
+
+name= input("whats is your name? ")
+print("Hello "+name)
+
+#type conversion 
+
+old_age= input("Enter your age : ")
+
+new_age= int(old_age)+2 
+
+print("Your age is : " + str(new_age))
