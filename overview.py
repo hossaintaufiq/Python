a7fd5bbe8f8689cd5da871dd25def1a754720681
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2025-04-15 20:59:23 +0600

    funtions

--- overview.py
+++ overview.py
@@ -1,1 +1,40 @@
-print("hello world ")
+# import sys
+# print (sys.version )
+# exit()
+
+# print("python basic  ")
+
+
+
+# if 5>2 :
+#     print("5 is greater than 2")
+#     if 4>3: 
+#         print("4 is greater than 3")
+#         if 5>4: 
+#             print("5 is greater than 4")
+# elif 6>5: 
+#     print("6 is greater than 5")
+
+# else:
+#     print("2 is greater than 5")
+
+# else: 
+#     print("2 is greater than 5")
+
+
+
+
+   
+
+# x,y,z = "orrange", "banana", "cherry"
+# print(x)
+# print(y) 
+# print(z)  
+
+x="awsome"
+
+def myfunc(): 
+    print("python is " + x)
+
+
+myfunc()
